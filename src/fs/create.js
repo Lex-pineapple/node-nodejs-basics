f66992e2f449b4d5f7efeb37304b64c5f908f637
Lex <pineapplepear55@gmail.com>
2023-06-18 22:34:00 +0300

    feat: solve all tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import path from 'path';
+import fsPromises from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { constants } from 'buffer';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files/fresh.txt');
+  const fileExists = await fsPromises.access(filePath, constants.F_OK).then(() => true).catch(() => false);
+  try {
+    if (fileExists) {
+      throw new Error('FS operation failed');
+    }
+    await fsPromises.writeFile(filePath, 'I am fresh and young');
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await create();
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import zlib from 'zlib';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
 const compress = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files/fileToCompress.txt');
+  const rs = fs.createReadStream(filePath);
+  const ws = fs.createWriteStream(path.join(__dirname, 'files/archive.gz'));
+  rs.on('error', (err) => {
+    console.error(err.message);
+  })
+
+  rs.pipe(zlib.createGzip()).pipe(ws);
 };
 
 await compress();
